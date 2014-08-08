899651fdb7d8bed4a41e79211cf83493e44a8e52
Xu Rong <xurongs@hotmail.com>
2014-08-09 01:04:38 +0800

    增加记录最后遥控码的功能。

--- src/remote.erl
+++ src/remote.erl
@@ -2,10 +2,11 @@
 -behaviour(gen_server).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export([start_link/0, start/0, stop/0]).
+-export([last/0]).
 
 -define(SERVER, ?MODULE).
 
--record(state, {uart}).
+-record(state, {uart, last = 0}).
 
 
 %%------------------------------------------------------------------------------
@@ -20,6 +21,9 @@ start_link() ->
 stop() -> 
 	gen_server:call(?MODULE, stop).
 
+last() ->
+	gen_server:call(?MODULE, last).
+
 %%------------------------------------------------------------------------------
 %% init
 %%------------------------------------------------------------------------------
@@ -33,6 +37,10 @@ init([]) ->
 %%------------------------------------------------------------------------------
 %% handle_call
 %%------------------------------------------------------------------------------
+handle_call(last, _, State) ->
+	#state{last = Last} = State,
+	{reply, {ok, {last, Last}}, State};
+
 handle_call(stop, _From, State) -> {stop, normal, stopped, State}.
 
 %%------------------------------------------------------------------------------
@@ -44,8 +52,8 @@ handle_cast(_Msg, State) -> {noreply, State}.
 %% handle_info
 %%------------------------------------------------------------------------------
 handle_info({_Uart, {data, Data}}, State) ->
-	recv_signal(Data),
-	{noreply, State};
+	SCode = recv_signal(Data),
+	{noreply, State#state{last = SCode}};
 
 handle_info(_Info, State) -> {noreply, State}.
 
@@ -64,11 +72,12 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 %% inner functions
 %%------------------------------------------------------------------------------
 recv_signal([0 | Data]) when length(Data) =:= 3 ->
-	Scode = list2num(Data),
-	proc_signal(Scode),
-	io:format("~w~n", [Scode]);
+	SCode = list2num(Data),
+	proc_signal(SCode),
+	io:format("~w~n", [SCode]),
+	SCode;
 recv_signal(_) ->
-	void.
+	-1.
 
 list2num(Data) ->
 	lists:foldl(fun(X, Sum) -> X + Sum * 256 end, 0, Data).
