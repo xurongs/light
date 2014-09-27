715675613f958706403de0d8d9e096c67f2894e8
Xu Rong <xurongs@hotmail.com>
2014-09-27 21:16:17 +0800

    解决ix服务端在未连接时对操作的记忆效应（消息积压）。

--- src/dev_ix.erl
+++ src/dev_ix.erl
@@ -3,10 +3,11 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export([start_link/2, start/2, stop/1]).
 -export([turn_on/2, turn_off/2, status/1]).
+-export([listen_and_accept_proc/2]).
 
 -define(SERVER, ?MODULE).
 
--record(state, {listen, socket, parent, cfg}).
+-record(state, {port, socket, parent, cfg}).
 
 
 %%------------------------------------------------------------------------------
@@ -36,10 +37,9 @@ status(Dev) ->
 init({Parent, Port, DevCfg}) ->
 	process_flag(trap_exit, true),
 
-	{ok, Listen} = gen_tcp:listen(Port, [binary, {packet, 4}, {reuseaddr, true}, {active, true}]),
-	self() ! {tcp_closed, undefined},
+	self() ! listen,
 
-	State = #state{listen = Listen, parent = Parent, cfg = DevCfg},
+	State = #state{port = Port, parent = Parent, cfg = DevCfg},
 	{ok, State}.
 
 %%------------------------------------------------------------------------------
@@ -63,20 +63,29 @@ handle_cast(_Msg, State) -> {noreply, State}.
 %%------------------------------------------------------------------------------
 %% handle_info
 %%------------------------------------------------------------------------------
-handle_info({tcp, _Socket, Bin}, State) ->
+handle_info({tcp, Socket, Bin}, State) ->
 	#state{parent = Parent} = State,
 	{status, Status} = binary_to_term(Bin),
 	Parent ! Status,
+	ok = inet:setopts(Socket, [{active, once}]),
 	{noreply, State};
 
 handle_info({tcp_closed, _Socket}, State) ->
-	#state{listen = Listen} = State,
-	io:format("The ix client is disconnected.~n"),
-	{ok, Socket} = gen_tcp:accept(Listen),
-	io:format("The ix client is connected.~n"),
+	self() ! listen,
+	{noreply, State#state{socket = undefined}};
+
+handle_info(listen, State) ->
+	#state{port = Port} = State,
+	listen_and_accept(Port),
+	{noreply, State};
+
+handle_info({tcp_accept, Socket}, State) ->
+	io:format("The ix client is connected.~n", []),
+	ok = inet:setopts(Socket, [{active, once}]),
 	{noreply, State#state{socket = Socket}};
 
-handle_info(_Info, State) -> {noreply, State}.
+handle_info(_Info, State) ->
+	{noreply, State}.
 
 %%------------------------------------------------------------------------------
 %% terminate
@@ -94,5 +103,20 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 %%------------------------------------------------------------------------------
 switch_light_proc(State, Type, Id) ->
 	#state{socket = Socket} = State,
-	gen_tcp:send(Socket, term_to_binary({Type, Id})).
+	case Socket of
+		undefined ->
+			ignore;
+		_ ->
+			gen_tcp:send(Socket, term_to_binary({Type, Id}))
+	end.
+
+listen_and_accept(Port) ->
+	spawn_link(?MODULE, listen_and_accept_proc, [self(), Port]).
+
+listen_and_accept_proc(Parent, Port) ->
+	{ok, Listen} = gen_tcp:listen(Port, [binary, {packet, 4}, {reuseaddr, true}, {active, false}]),
+	{ok, Socket} = gen_tcp:accept(Listen),
+	ok = gen_tcp:controlling_process(Socket, Parent),
+	Parent ! {tcp_accept, Socket},
+	gen_tcp:close(Listen).
 
